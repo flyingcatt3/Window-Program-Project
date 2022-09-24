f622b430a7e398ef59c6cab5527215c0528eeb54
lazzicat <jones184189@gmail.com>
2022-09-24 21:54:44 +0800

    20220924

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -25,19 +25,22 @@ Partial Class Form1
         Me.Start = New System.Windows.Forms.Button()
         Me.GameTitle = New System.Windows.Forms.Label()
         Me.ver = New System.Windows.Forms.Label()
+        Me.StartLayout = New System.Windows.Forms.TableLayoutPanel()
+        Me.StartLayout.SuspendLayout()
         Me.SuspendLayout()
         '
         'Start
         '
+        Me.Start.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Start.AutoSize = True
-        Me.Start.FlatAppearance.BorderColor = System.Drawing.Color.Orange
+        Me.Start.FlatAppearance.BorderColor = System.Drawing.Color.Silver
         Me.Start.FlatAppearance.BorderSize = 2
         Me.Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Start.Font = New System.Drawing.Font("Taipei Sans TC Beta", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Start.ForeColor = System.Drawing.Color.MediumSeaGreen
-        Me.Start.Location = New System.Drawing.Point(550, 650)
+        Me.Start.Location = New System.Drawing.Point(3, 385)
         Me.Start.Name = "Start"
-        Me.Start.Size = New System.Drawing.Size(500, 100)
+        Me.Start.Size = New System.Drawing.Size(858, 100)
         Me.Start.TabIndex = 0
         Me.Start.Text = "開始遊戲"
         Me.Start.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage
@@ -45,13 +48,14 @@ Partial Class Form1
         '
         'GameTitle
         '
+        Me.GameTitle.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.GameTitle.AutoSize = True
         Me.GameTitle.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.GameTitle.Font = New System.Drawing.Font("Taipei Sans TC Beta", 54.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.GameTitle.ImageAlign = System.Drawing.ContentAlignment.TopCenter
-        Me.GameTitle.Location = New System.Drawing.Point(568, 148)
+        Me.GameTitle.Location = New System.Drawing.Point(3, 93)
         Me.GameTitle.Name = "GameTitle"
-        Me.GameTitle.Size = New System.Drawing.Size(464, 104)
+        Me.GameTitle.Size = New System.Drawing.Size(858, 104)
         Me.GameTitle.TabIndex = 1
         Me.GameTitle.Text = "(遊戲名稱)"
         Me.GameTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter
@@ -59,25 +63,44 @@ Partial Class Form1
         '
         'ver
         '
+        Me.ver.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ver.AutoSize = True
+        Me.ver.BackColor = System.Drawing.Color.Transparent
+        Me.ver.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ver.ForeColor = System.Drawing.Color.White
         Me.ver.Location = New System.Drawing.Point(1496, 833)
         Me.ver.Name = "ver"
-        Me.ver.Size = New System.Drawing.Size(74, 23)
+        Me.ver.Size = New System.Drawing.Size(76, 23)
         Me.ver.TabIndex = 2
         Me.ver.Text = "Label1"
+        '
+        'StartLayout
+        '
+        Me.StartLayout.ColumnCount = 1
+        Me.StartLayout.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.StartLayout.Controls.Add(Me.Start, 0, 1)
+        Me.StartLayout.Controls.Add(Me.GameTitle, 0, 0)
+        Me.StartLayout.Location = New System.Drawing.Point(371, 126)
+        Me.StartLayout.Name = "StartLayout"
+        Me.StartLayout.RowCount = 2
+        Me.StartLayout.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.StartLayout.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.StartLayout.Size = New System.Drawing.Size(864, 580)
+        Me.StartLayout.TabIndex = 3
         '
         'Form1
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit
         Me.AutoSize = True
         Me.ClientSize = New System.Drawing.Size(1582, 853)
+        Me.Controls.Add(Me.StartLayout)
         Me.Controls.Add(Me.ver)
-        Me.Controls.Add(Me.GameTitle)
-        Me.Controls.Add(Me.Start)
         Me.ForeColor = System.Drawing.SystemColors.ControlText
         Me.Name = "Form1"
         Me.RightToLeft = System.Windows.Forms.RightToLeft.No
         Me.Text = "Project"
+        Me.StartLayout.ResumeLayout(False)
+        Me.StartLayout.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -86,4 +109,5 @@ Partial Class Form1
     Friend WithEvents Start As Button
     Friend WithEvents GameTitle As Label
     Friend WithEvents ver As Label
+    Friend WithEvents StartLayout As TableLayoutPanel
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,27 +1,175 @@
-﻿Imports WinFormsApp1.Argus.Audio
+﻿Public Class Form1
 
-Public Class Form1
     Dim Path = Application.StartupPath
     ReadOnly startBGM = Path + "start.mp3"
-    Dim audioPlayer As New AudioFile()
+    ReadOnly StoryListBG = Path + "StoryListBG.jpg"
+    ReadOnly Clicksound = Path + "click.mp3"
+    Dim StoryList As New Panel()
+    Dim StoryListTitle As New Label()
 
+    Public Sub timeDelay(ByVal secondsDelayedBy As Double)
+        Dim stopwatch As New Stopwatch
+        Dim endtimer = False
+        stopwatch.Start()
+        Do Until endtimer = True
+            If stopwatch.ElapsedMilliseconds > (secondsDelayedBy * 1000) Then
+                endtimer = True
+                stopwatch.Stop()
+            End If
+        Loop
+    End Sub
+
+    Private Declare Function mciSendString Lib "winmm.dll" Alias "mciSendStringA" _
+    (ByVal lpstrCommand As String, ByVal lpstrReturnString As String,
+    ByVal uReturnLength As Integer, ByVal hwndCallback As Integer) As Integer
+
+    Sub setstartclr(r, g, b)
+        Start.ForeColor = Color.FromArgb(r, g, b)
+        timeDelay(0.01)
+        'ver.Text = r.ToString + "," + g.ToString + "," + b.ToString
+    End Sub
+
+    'https://www.it-sideways.com/2014/04/vbnet-form-with-background-image.html
+    Protected Overrides ReadOnly Property CreateParams() As Windows.Forms.CreateParams
+        Get
+            Dim cp As CreateParams = MyBase.CreateParams
+            cp.ExStyle = cp.ExStyle Or &H2000000
+            Return cp
+        End Get
+    End Property
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        PlayMusic(startBGM)
-        ver.Text = "20220922"
-    End Sub
-    Sub PlayMusic(sender)
-        audioPlayer.Filename = sender
-        audioPlayer.Play()
+
+        Me.SetStyle(ControlStyles.OptimizedDoubleBuffer, True)
+        Me.SetStyle(ControlStyles.AllPaintingInWmPaint, True)
+
+        '初始化所需控制項的格式
+        StartLayout.Location = New Point(Convert.ToInt32(Me.ClientSize.Width / 2 - Me.StartLayout.Width / 2),
+                                       Convert.ToInt32(Me.ClientSize.Height / 2 - Me.StartLayout.Height / 2))
+        ver.Text = "20220924"
+        ver.BackColor = Color.FromArgb(100, 0, 0, 0)
+
+        StoryList.Location = Me.Size / 20
+        StoryList.Width = Me.Size.Width * 0.9
+        StoryList.Height = Me.Size.Height * 0.9
+        StoryList.BackColor = Color.FromArgb(120, 0, 0, 0)
+
+        StoryListTitle.Location = Me.Size / 20
+        StoryListTitle.Text = "選擇故事"
+        StoryListTitle.Anchor = Drawing.ContentAlignment.TopLeft
+        StoryListTitle.Font = New Font("Taipei Sans TC Beta", 36, FontStyle.Bold)
+        StoryListTitle.ForeColor = Color.White
+        StoryListTitle.BackColor = StoryList.BackColor
+        StoryListTitle.AutoSize = True
+
+        mciSendString("open """ & startBGM & """ alias startBGM", Nothing, 0, IntPtr.Zero)
+        mciSendString("open """ & Clicksound & """ alias ClickSound", Nothing, 0, IntPtr.Zero)
+        mciSendString("play startBGM repeat", String.Empty, 0, 0)
+        mciSendString("setaudio startBGM volume to 200", 0, 0, 0)
+
     End Sub
 
-    Private Sub Start_Click(sender As Object, e As EventArgs) Handles Start.Click
-        audioPlayer.Stop()
-        Start.Dispose()
-        GameTitle.Dispose()
+    Private Sub buttonClickHandler() Handles MyBase.Load
+        For Each ctrl In Me.StartLayout.Controls
+            If (TypeOf ctrl Is Button) Then _
+           AddHandler DirectCast(ctrl, Button).Click, AddressOf ButtonClick
+        Next
+
+        'AddHandler StoryList.Click, AddressOf ButtonClick
+
     End Sub
 
-    Private Sub NextSentence(sender As Object, e As EventArgs) Handles MyBase.Click
+    'https://stackoverflow.com/questions/48710165/back-colour-of-button-not-changing-when-updated-in-for-loop
+    Public Async Sub StartColorCycle(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim r As Integer = Start.ForeColor.R
+        Dim g As Integer = Start.ForeColor.G
+        Dim b As Integer = Start.ForeColor.B
+
+        Await Task.Run(
+            Sub()
+                Do While StartLayout.Visible
+
+                    For x = g To r Mod 255
+                        g += 1
+                        setstartclr(r, g, b)
+                    Next
+
+                    For x = b Mod 255 + 1 To r
+                        r -= 1
+                        setstartclr(r, g, b)
+                    Next
+
+                    For x = b To g Mod 255
+                        b += 1
+                        setstartclr(r, g, b)
+                    Next
+
+                    For x = r Mod 255 + 1 To g
+                        g -= 1
+                        setstartclr(r, g, b)
+                    Next
+
+                    For x = g To b Mod 255
+                        r += 1
+                        setstartclr(r, g, b)
+                    Next
+
+                    For x = g Mod 255 + 1 To b
+                        b -= 1
+                        setstartclr(r, g, b)
+                    Next
+
+                Loop
+
+            End Sub)
 
     End Sub
+
+    Sub ButtonClick()
+
+        mciSendString("play ClickSound", String.Empty, 0, 0)
+        timeDelay(0.25)
+        mciSendString("seek ClickSound to start", String.Empty, 0, 0)
+        'MsgBox("hello")
+
+    End Sub
+    Private Sub ChooseStory() Handles Start.Click
+
+        StartLayout.Hide()
+
+        Me.BackgroundImage = Image.FromFile(StoryListBG)
+        Me.BackgroundImageLayout = ImageLayout.Stretch
+
+        Me.Controls.Add(StoryListTitle)
+        Me.Controls.Add(StoryList)
+
+        'TODO:增加選擇故事的功能
+
+    End Sub
+
+    '使用者調整視窗大小後格式化畫面上的控制項以符合當前視窗
+    Private Sub ControlLayout(sender As Object, e As EventArgs) Handles Me.Resize
+
+        If StoryList.Visible Then
+            StoryList.Location = Me.Size / 20
+            StoryList.Width = Me.Size.Width * 0.9
+            StoryList.Height = Me.Size.Height * 0.9
+
+            StoryListTitle.Location = Me.Size / 20
+        End If
+
+        If StartLayout.Visible Then
+            StartLayout.Location = New Point(Convert.ToInt32(Me.ClientSize.Width / 2 - Me.StartLayout.Width / 2),
+                                       Convert.ToInt32(Me.ClientSize.Height / 2 - Me.StartLayout.Height / 2))
+        End If
+    End Sub
+
+    Private Sub ver_Click(sender As Object, e As EventArgs) Handles ver.Click
+
+    End Sub
+
+    'Private Sub NextSentence(sender As Object, e As EventArgs) Handles MyBase.Click
+
+    'End Sub
 
 End Class
